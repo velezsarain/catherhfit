44953bb44a8d7f660e0df6d5c9bb373da05fe06b
Shervin Sarain <shervin.sarain@labri.fr>
2019-01-15 23:23:11 +0100

    added hour markers

--- app/index.ts
+++ app/index.ts
@@ -3,18 +3,20 @@ import document from 'document'
 // import { preferences } from "user-settings";
 
 // Update the clock every minute
-clock.granularity = 'minutes'
+clock.granularity = 'seconds'
 
 // Get a handle on the <text> element
-const timeDisplay = document.getElementById('label')
-let today: Date
-let mins: number
-let hours: number
+
 
 // Update the <text> element every tick with the current time
 clock.ontick = (evt) => {
-    today = evt.date
-    hours = today.getHours()
-    mins = today.getMinutes()
-    timeDisplay.text = `${hours}:${mins}`
+
 }
+
+// diameter = 625
+// 1% = 6.25
+// 12 3 6 9 H = 44 -> 7% = 21px
+// 12 3 6 9 W = 22 -> 3.5% = 10
+
+// H  51.84
+// W 6 OR 12
