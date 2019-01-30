6abb601295ec0efbfd97b0fb47e129c9682e10f2
Shervin Sarain <shervin.sarain@labri.fr>
2019-01-30 08:45:19 +0100

    added images

--- app/index.ts
+++ app/index.ts
@@ -1,30 +1,67 @@
 import clock from 'clock'
+import { HeartRateSensor } from "heart-rate";
+import { BodyPresenceSensor } from "body-presence";
+import { display } from "display";
 import document from 'document'
+import { weekDays } from '../common/const'
 // import { preferences } from "user-settings";
 
-// update the clock every minute
-clock.granularity = 'seconds'
 const hoursHand = document.getElementById('hours-hand') as GroupElement
 const minutesHand = document.getElementById('minutes-hand') as GroupElement
 const secondsHand = document.getElementById('seconds-hand') as GroupElement
+const dateTextField = document.getElementById('date-text') as TextElement
+const heartRateText = document.getElementById('hr-text') as TextElement
+const heartRateIcon = document.getElementById('heart-icon') as ImageElement
+
+const heartSensor = new HeartRateSensor()
+const bodySensor = new BodyPresenceSensor()
+heartSensor.start()
+
+
 let hours: number
 let minutes: number
 let seconds: number
+let day: number
+let weekDaysArray: string[]
+
+// set initial variable values
+clock.granularity = 'seconds'
+weekDaysArray = weekDays.en.split('|')
+day = -1
 
 // clock update loop
 clock.ontick = (evt) => {
-    hours = evt.date.getHours() % 12
-    minutes = evt.date.getMinutes()
-    seconds = evt.date.getSeconds()
-    hoursHand.groupTransform!.rotate.angle = (hours + minutes / 60 + seconds / 3600) * 30
-    minutesHand.groupTransform!.rotate.angle = minutes * 6 + seconds * 0.1
-    secondsHand.groupTransform!.rotate.angle = seconds * 6
+    if (display.on) {
+        hours = evt.date.getHours() % 12
+        minutes = evt.date.getMinutes()
+        seconds = evt.date.getSeconds()
+        hoursHand.groupTransform!.rotate.angle = (hours + minutes / 60 + seconds / 3600) * 30
+        minutesHand.groupTransform!.rotate.angle = (minutes + seconds / 60) * 6
+        secondsHand.groupTransform!.rotate.angle = seconds * 6
+
+        //console.log(heartSensor.heartRate)
+
+        // if day has changed
+        if (day !== evt.date.getDate()) {
+            day = evt.date.getDate()
+            dateTextField.text = weekDaysArray[evt.date.getDay()] + ' ' + day
+        }
+
+        // if(heartSensor.activated){
+        //     heartRateIcon.animbuiate('enable');
+        // }
+
+        // if (!heartSensor.activated && bodySensor.present) {
+        //     heartSensor.start()
+        // }
+
+        // if (!bodySensor.present) {
+        //     heartSensor.stop()
+        // }
+    }
 }
 
-// font size at 50px -> H 36px W 26px | 32px margin from outside
-// diameter = 625
-// 1% = 6.25
-// numbered-hour-marker -> W 7% 21px | H 3.5% 10px
-// hour-marker -> W 4% 12px | H 16% 50px
-// minute-marker -> W 1% 3px | H 5% 15px
-// seconds-hand -> W 2px | L 62% 186 | Bottom to center 16% 48px
+//heart rate update loop
+// heartSensor.onreading = () => {
+//     console.log(heartSensor.heartRate)
+// }
---
+++ common/const.ts
@@ -0,0 +1,17 @@
+export enum themes {
+
+}
+
+export enum weekDays {
+    en = 'Sun|Mon|Tue|Wed|Thu|Fri|Sat',
+    es = 'Dom|Lun|Mar|Mié|Jue|Vie|Sáb',
+    fr = 'Dim|Lun|Mar|Mer|Jeu|Ven|Sam',
+    de = 'So|Mo|Di|Mi|Do|Fr|Sa',
+    it = 'Dom|Lun|Mar|Mer|Gio|Ven|Sab',
+    ja = '日|月|火|水|木|金|土',
+    ko = '일|월|화|수|목|금|토',
+    nl = 'Zo|Ma|Di|Wo|Do|Vr|Za',
+    sv = 'Sön|Mån|Tis|Ons|Tors|Fre|Lör',
+    zhcn = '周日|周一|周二|周三|周四|周五|周六',
+    zhtw = '週日|週一|週二|週三|週四|週五|週六'
+}
